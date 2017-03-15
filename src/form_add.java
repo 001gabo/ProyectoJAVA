c7891b3d47c5e2eddbea4d92daf44d8391c2474e
roberto <roberto.menivar.hz@gmail.com>
2017-03-14 22:54:50 -0600

    modificando el agregar

--- src/form_add.java
+++ src/form_add.java
@@ -28,6 +28,7 @@ public class form_add extends javax.swing.JFrame {
   PreparedStatement pst;//query
   ResultSet rs=null;//rsultado
   Statement s;
+  crud_libros open;
  
   
   
@@ -59,9 +60,7 @@ public class form_add extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         in_edicion = new javax.swing.JTextField();
-        in_id = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
         in_editorial = new javax.swing.JTextField();
         in_name = new javax.swing.JTextField();
         jLabel7 = new javax.swing.JLabel();
@@ -111,12 +110,7 @@ public class form_add extends javax.swing.JFrame {
 
         in_edicion.setName("in_edicion"); // NOI18N
 
-        in_id.setName("in_id"); // NOI18N
-
         jLabel6.setText("Edición");
-
-        jLabel1.setText("Identificador");
-        jLabel1.setName("lb_id"); // NOI18N
 
         in_editorial.setName("in_editorial"); // NOI18N
 
@@ -182,7 +176,6 @@ public class form_add extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(50, 50, 50)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
                             .addComponent(jLabel2)
                             .addComponent(jLabel4)
                             .addComponent(jLabel5)
@@ -200,7 +193,7 @@ public class form_add extends javax.swing.JFrame {
                             .addComponent(in_cod, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(in_estado, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
                                 .addComponent(btn_estado, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
@@ -211,14 +204,12 @@ public class form_add extends javax.swing.JFrame {
                                     .addComponent(in_editorial, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 92, Short.MAX_VALUE)
                                     .addComponent(in_anio, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 92, Short.MAX_VALUE)
                                     .addComponent(in_categoria, javax.swing.GroupLayout.Alignment.LEADING))
-                                .addGap(18, 18, 18)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                    .addComponent(btn_author, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(btn_categorianew, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(btn_editorial, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addComponent(in_id, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 95, Short.MAX_VALUE)
-                                .addComponent(in_name, javax.swing.GroupLayout.Alignment.LEADING))))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(btn_categorianew, javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(btn_author, javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(btn_editorial, javax.swing.GroupLayout.Alignment.TRAILING)))
+                            .addComponent(in_name, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(128, 128, 128)
                         .addComponent(jButton1)
@@ -228,11 +219,7 @@ public class form_add extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(22, 22, 22)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(in_id, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(53, 53, 53)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(in_name, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
@@ -290,11 +277,17 @@ public class form_add extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
         private void mostrar(){    
         
-//        variables a esconder
-        in_categoria.setVisible(false);   
+        
+//        variables a esconder y desabilitar
+        in_categoria.setVisible(false); 
+        in_categoria.setEnabled(false);
         in_author.setVisible(false);
+        in_categoria.setEnabled(false);
         in_editorial.setVisible(false);
+        in_editorial.setEnabled(false);
         in_estado.setVisible(false);
+        in_estado.setEnabled(false);
+        
         
         combo_author=new JComboBox();
         combo_author.removeAllItems(); 
@@ -304,7 +297,7 @@ public class form_add extends javax.swing.JFrame {
         combo_editorial.removeAllItems(); 
         combo_estados=new JComboBox();
         combo_estados.removeAllItems(); 
-
+        
      //  JOptionPane.showMessageDialog(null,in_estado.getLocation());   
         try{
             
@@ -359,6 +352,8 @@ public class form_add extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(null,("error"+ex));
         }
     }
+    
+    
         
         
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
@@ -379,17 +374,83 @@ public class form_add extends javax.swing.JFrame {
           String pivote=combo_author.getSelectedItem().toString();
           int valor=pivote.indexOf(" "); 
           String nombre=pivote.substring(0,valor); 
-          String id_autor="";
+          int id_autor=0;
           rs=s.executeQuery("select id_autor from autor where aut_name='"+nombre+"'");
           while(rs.next())
             { 
-            id_autor=rs.getString(1);
+            id_autor=rs.getInt(1);
             }
           
-                JOptionPane.showMessageDialog(null,id_autor);
-        //rs=s.executeQuery("select id_categoria from categoria where cat_categoria'"++"'"); 
-        //        pst=con.prepareStatement("insert into libros(id_lib, lib_descrip, lib_name, lib_cantidad, lib_categoria, lib_autor, lib_edicion,lib_editorial,lib_year,lib_observacion) values()");
-        //        
+     //          encontrando el id de la editorial
+         int id_editorial=0;
+          rs=s.executeQuery("select id_editorial from editoriales where ed_name='"+(combo_editorial.getSelectedItem().toString())+"'");
+          while(rs.next())
+            { 
+            id_editorial=rs.getInt(1);
+            }
+          
+          
+     //          encontrando el id del estado  
+         int id_estado=0;
+          rs=s.executeQuery("select id_estados from estados where es_estados='"+(combo_estados.getSelectedItem().toString())+"'");
+          while(rs.next())
+            { 
+            id_estado=rs.getInt(1);
+            } 
+          
+          
+   // Ahora haremos el insert principal
+          
+        
+          
+       
+        pst=con.prepareStatement("insert into libros(lib_descrip, lib_name, lib_cantidad, lib_categoria, lib_autor, lib_edicion,lib_editorial,lib_year,lib_codigo,lib_observacion,lib_estado) values(?,?,?,?,?,?,?,?,?,?,?,?)");
+       
+        pst.setString(1,in_desc.getText());
+        pst.setString(2,in_name.getText());
+        pst.setInt(3,Integer.parseInt(in_cantidad.getText()));
+        
+        if(in_categoria.getDragEnabled()==true)
+        {    
+        pst.setInt(4,Integer.parseInt(in_categoria.getText()));
+        }
+        else{pst.setInt(4,id_categoria);}
+        
+        if(in_author.getDragEnabled()==true)
+        {    
+        pst.setInt(5,Integer.parseInt(in_author.getText()));
+        }
+        else{ pst.setInt(5,id_autor);}
+        
+        
+       
+        pst.setString(6,in_edicion.getText());
+        
+        if(in_edicion.getDragEnabled()==true)
+        {    
+        pst.setInt(7,Integer.parseInt(in_edicion.getText()));
+        }
+        else{ pst.setInt(7,id_editorial);}
+        
+        
+        pst.setInt(8,Integer.parseInt(in_anio.getText()));
+        pst.setString(9,in_cod.getText());
+        pst.setString(10,in_observacion.getText());
+       
+        if(in_edicion.getDragEnabled()==true)
+        {    
+        pst.setInt(11,Integer.parseInt(in_estado.getText()));
+        }
+        else{  pst.setInt(11,id_estado);}
+        
+        pst.executeUpdate();
+        
+        open =new crud_libros();
+        open.setVisible(true);
+         this.setVisible(false);
+        
+        
+        
         }
         catch(Exception ex){
             
@@ -400,6 +461,7 @@ public class form_add extends javax.swing.JFrame {
 
     private void btn_categorianewActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_categorianewActionPerformed
       in_categoria.setVisible(true);
+      in_categoria.setEnabled(true);
       combo_categoria.setVisible(false);
       btn_categorianew.setVisible(false);
     }//GEN-LAST:event_btn_categorianewActionPerformed
@@ -410,18 +472,23 @@ public class form_add extends javax.swing.JFrame {
 
     private void btn_authorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_authorActionPerformed
       in_author.setVisible(true);
+      in_author.setEnabled(true);
       combo_author.setVisible(false);
       btn_author.setVisible(false);
+      
+      
     }//GEN-LAST:event_btn_authorActionPerformed
 
     private void btn_editorialActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_editorialActionPerformed
       in_editorial.setVisible(true);
+      in_editorial.setEnabled(true);
       combo_editorial.setVisible(false);
       btn_editorial.setVisible(false);
     }//GEN-LAST:event_btn_editorialActionPerformed
 
     private void btn_estadoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_estadoActionPerformed
        in_estado.setVisible(true);
+       in_estado.setEnabled(true);
       combo_estados.setVisible(false);
       btn_estado.setVisible(false);
     }//GEN-LAST:event_btn_estadoActionPerformed
@@ -476,11 +543,9 @@ public class form_add extends javax.swing.JFrame {
     private javax.swing.JTextField in_edicion;
     private javax.swing.JTextField in_editorial;
     private javax.swing.JTextField in_estado;
-    private javax.swing.JTextField in_id;
     private javax.swing.JTextField in_name;
     private javax.swing.JTextField in_observacion;
     private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel12;
