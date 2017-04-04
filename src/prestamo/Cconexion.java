61e95ed64ecc4015dc47d9124758ba93769b3eeb
hugo-palma <hugodobe_@hotmail.com>
2017-04-03 23:22:38 -0600

    Update Cconexion.java

--- src/prestamo/Cconexion.java
+++ src/prestamo/Cconexion.java
@@ -9,6 +9,7 @@ package prestamo;
  *
  * @author hugo
  */
+import com.sun.javafx.scene.control.skin.VirtualFlow;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,37 +21,76 @@ public class Cconexion {
     {
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance(); 
-            Connection conexion= DriverManager.getConnection("jdbc:mysql://localhost:3306/db_authors","root","");
+            Connection conexion= DriverManager.getConnection("jdbc:mysql://localhost:3306/biblioteca","root","");
             con = conexion;
         }
         catch (Exception e) {
-            
         }    
     }
     public Connection getconector()
     {
         return con;
     }
-    public List<String[]> bucscarpornom(String nombre)
+    public int num_coin_buscar(String parametro, int opcion)
     {
-        List<String[]> lista = new ArrayList<String[]>();
-        String [] datos = new String[14];
+        int numero = 0;
+        String query = "";
+        try{
+            switch(opcion)
+            {
+                case 1:
+                    query = "CALL count_coincidencias_busq_libros_nombre ('" + parametro + "')";
+                    break;
+                case 2:
+                    query = "CALL count_coincidencias_busq_libros_autor ('" + parametro + "')";
+                    break;
+                case 3:
+                    query = "CALL count_coincidencias_busq_libros_editorial ('" + parametro + "')";
+                    break;
+            }
+            Statement sentencia = con.createStatement();
+            ResultSet resp = sentencia.executeQuery(query);
+            while(resp.next())
+            {
+                numero = resp.getInt(1);
+            }
+            return numero;
+        }
+        catch(Exception e)
+        {
+            System.err.println(e);
+        }
+        return numero;
+    }
+    public List<List<String>> bucscarpornom(String parametro, int indice, int opcion)
+    {
+        List<List<String>> lista = new ArrayList<List<String>>();
+        List<String> datos = new ArrayList<String>();
         try {
-            String query = "SELECT * from libros WHERE lib_name LIKE '%" + nombre + "%'";
-            //datos[0] = query;
-            //lista.add(datos);
-            System.out.println(query);
+            String query = "";
+            switch(opcion)
+            {
+                case 1:
+                    query = "CALL buscar_libros_nombre ('" + parametro + "', '" + indice + "')";
+                    break;
+                case 2:
+                    query = "CALL buscar_libros_autor ('" + parametro + "', '" + indice + "')";
+                    break;
+                case 3:
+                    query = "CALL buscar_libros_editorial ('" + parametro + "', '" + indice + "')";
+                    break;
+            }
             int temporal;
             Statement sentencia = con.createStatement();
             ResultSet resp = sentencia.executeQuery(query);
             while(resp.next())
             {
-                temporal = resp.getInt(1);
-                datos[0] = Integer.toString(temporal);
-                datos[1] = resp.getString(2);
-                datos[2] = resp.getString(3);
-                datos[3] = resp.getString(4);
-             
+                temporal = resp.getInt("id_lib");
+                datos.add(Integer.toString(temporal));
+                datos.add(resp.getString("lib_name"));
+                datos.add(resp.getString("aut_name"));
+                datos.add(resp.getString("ed_name"));
+                
                 //datos[4] = resp.getInt(5)
                 lista.add(datos);
                 //con.close();
@@ -58,7 +98,6 @@ public class Cconexion {
         } catch (Exception e) {
             JOptionPane.showConfirmDialog(null, e);
         }
-        
         return lista;
     }
 }
